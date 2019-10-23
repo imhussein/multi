e3d1e4bd0b2be2bb1757f285210c8ca127321394
mh10445 <hussein@blockgemini.com>
2019-10-23 12:31:55 +0400

    Setup

---
+++ server/index.js
@@ -0,0 +1,65 @@
+const express = require("express");
+const redis = require("redis");
+const { Pool } = require("pg");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const {
+  PG_DATABASE,
+  PG_HOST,
+  PG_PORT,
+  PG_USER,
+  PG_PASSWORD,
+  REDIS_PORT,
+  REDIS_URL
+} = require("./keys");
+const redisClient = redis.createClient({
+  host: REDIS_URL,
+  port: REDIS_PORT,
+  retry_strategy: () => 1000
+});
+const redisPublisher = redisClient.duplicate();
+const conn = new Pool({
+  port: PG_PORT,
+  user: PG_USER,
+  password: PG_PASSWORD,
+  database: PG_DATABASE,
+  host: PG_HOST
+});
+conn.on("error", function() {
+  console.log(`Failed To Connect TO POSTGRES`);
+});
+conn.query("CREATE TABLE IF NOT EXISTS values (Number INT)").catch(err => {
+  console.log(err);
+});
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.get("/", function(req, res) {
+  res.send("success");
+});
+
+app.get("/values/all", async (req, res) => {
+  const values = await conn.query("SELECT * FROM values");
+  res.send(values.rows);
+});
+
+app.get("/values/current", async (req, res) => {
+  redisClient.hgetall("values", (err, values) => {
+    res.send(values);
+  });
+});
+
+app.post("/values", async (req, res) => {
+  const { index } = req.body;
+  if (parseInt(index) > 40) {
+    return res.status(422).send("VALUES IS HIGH");
+  }
+  redisClient.hset("values", index, "NOT YET");
+  redisPublisher.publish("insert", index);
+  res.send({ working: true });
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server Connected On PORT ${port}`);
+});
---
+++ server/keys.js
@@ -0,0 +1,9 @@
+module.exports = {
+  PG_PORT: process.env.PG_PORT,
+  PG_HOST: process.env.PG_HOST,
+  PG_USER: process.env.PG_USER,
+  PG_PASSWORD: process.env.PG_PASSWORD,
+  PG_DATABASE: process.env.PG_DATABASE,
+  REDIS_URL: process.env.REDIS_URL,
+  REDIS_PORT: process.env.REDIS_PORT
+};
